f291c03f6f3e7f6e3fc5e460fd532aa564ecb2ac
bokuweb <bokuweb12@gmail.com>
2016-02-12 16:45:35 +0900

    Rename app

--- App.js
+++ App.js
@@ -1,62 +1,66 @@
-/* eslint-env es6 */
-var React = require('react')
-var Relay = require('react-relay')
+import React from 'react';
+import Relay from 'react-relay';
 
-// A simple top-level component that illustrates how to render Relay-fetched
-// data using props. In this case Relay will populate a `user` property that
-// has a collection of `widgets` based on the queries and fragments we give it
-// further below.
 class App extends React.Component {
   render() {
+    return(
+      <div className="container">
+        <h2>{this.props.user.name} Conferences</h2>
+        {this.props.user.conferences.edges.map(edge =>
+          <Conference edge={edge} />
+        )}
+      </div>
+    );
+  }  
+}
+
+
+class Conference extends React.Component {
+  render() {
+    var edge = this.props.edge;
     return (
-      <div>
-        <h2>User: {this.props.user.name}</h2>
-        <h2>Widgets:</h2>
-        <ul>
-          {/* In schema/schema.js we define a Connection between users and widgets */}
-          {/* Connections use `edges` and `node` to hold paging info and child items */}
-          {this.props.user.widgets.edges.map(edge =>
-            <li key={edge.node.id}>{edge.node.name} (Global ID: {edge.node.id})</li>
-          )}
-        </ul>
+      <div className="col-sm-4">
+        <div className="panel panel-default" key={edge.node.id}>
+          <div className="panel-heading">
+            <h3>{edge.node.name}</h3>
+          </div>
+          <div className="panel-body">
+            {edge.node.description}
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-// The component we need to export is a Relay wrapper around our App component
-// from above. It declares the GraphQL fragments where we list the properties
-// we want to be fetched – eg, user.name, user.widgets.edges, etc
 exports.Container = Relay.createContainer(App, {
+  initialVariables: {
+    userToShow: 2
+  },
   fragments: {
-    // The property name here reflects what is added to `this.props` above.
-    // This template string will be parsed by babel-relay-plugin when we browserify.
     user: () => Relay.QL`
       fragment on User {
         name,
-        widgets(first: 10) {
+        conferences(userToShow: $userToShow) {
           edges {
             node {
               id,
               name,
+              description
             },
           },
         },
       }
-    `,
+    `
   },
-})
+});
 
-// The Relay root container needs to know what queries will occur at the top
-// level – these configurations are currently called Routes in Relay, but this
-// name is misleading and under review so we don't use it here.
 exports.queries = {
-  name: 'AppQueries', // can be anything, just used as an identifier
+  name: 'ConferenceQueries',
   params: {},
   queries: {
-    // We can use this shorthand so long as the component we pair this with has
-    // a fragment named "user", as we do above.
-    user: () => Relay.QL`query { user }`,
+    // user in this case matches the fragment in the container above
+    user: () => Relay.QL`query { user }`   
   },
 }
 
--- browser.js
+++ browser.js
@@ -1,13 +1,12 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-var Relay = require('react-relay')
-var ConferenceApp = require('./ConferenceApp')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Relay from 'react-relay';
+import App from './app';
 
 ReactDOM.render(
   <Relay.RootContainer 
-     Component={ConferenceApp.Container}
-     route={ConferenceApp.queries}
+     Component={App.Container}
+     route={App.queries}
      onReadyStateChange={({error}) => { if (error) console.error(error) }} />,
-
   document.getElementById('content')  
 )
